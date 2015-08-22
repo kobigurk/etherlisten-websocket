627f8d035e8afabfc299bbb762d1f290d9b80e1b
Kobi Gurkan <kobigurk@gmail.com>
2015-08-22 19:22:07 +0300

    error handling

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var wss = new WebSocketServer({host: '0.0.0.0', port:8085});
 
 wss.broadcast = function broadcast(data) {
       wss.clients.forEach(function each(client) {
-              client.send(JSON.stringify(data));
+          try {
+              client.send(JSON.stringify(data), function (err) {
+                  console.log(err.message);
+              });
+          } catch (e) {
+              console.log(e.message);
+          }
       });
 };
 wss.on('connection', function (ws) {
